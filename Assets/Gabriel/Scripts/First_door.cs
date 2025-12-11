0618cbbf67001d0cf07ad8d8c8ecaf5d27a675dc
eclecticxc <163816810+eclecticxc@users.noreply.github.com>
2025-12-10 18:10:22 -0800

    Maze is done/Creating new script for first door

---
+++ Assets/Gabriel/Scripts/First_door.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class First_door : MonoBehaviour
+{
+    //Start is called before the fist frame update 
+    public int redLocks = 1;  //how many keys needed to open this door
+    private void OnCollisionEnter(Collision other)
+    {
+        //Check if player collided with this door 
+        if (other.gameObject.tag == "Player")
+        {
+            //stores a refence to the player script 
+            Player playerScript = other.gameObject.GetComponent<Player>();
+
+            //Check if player key is >= this doors number of locks 
+            if (playerScript.redKeys >= redLocks)
+            {
+                print("working door");
+                //reduce player key amount and destroy door
+                playerScript.redKeys -= redLocks;
+                Destroy(gameObject);
+
+
+            }
+
+
+
+
+
+
+        }
+    }
